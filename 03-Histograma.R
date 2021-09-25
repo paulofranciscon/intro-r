aa01db9fb242d7490656de10f83d0be51721328a
Paulo Franciscon <57269765+paulofranciscon@users.noreply.github.com>
2021-09-25 17:42:10 -0300

    Update 03-Histograma.R

--- 03-Histograma.R
+++ 03-Histograma.R
@@ -1,4 +1,4 @@
-# HIstograma - grafico de contagem e de distribuićão de frequencia. Confundível com o grafico de barras
+# Histograma - gráfico de contagem e de distribuição de frequência. Confundível com o gráfico de barras
 
 setwd("/media/paulo/DISC2/PUC/5 INTRODUCAO LINGUAGEM R/SCRIPTS/")
 getwd()
@@ -12,22 +12,22 @@ View(cars)
 dados = cars$speed
 dados
 
-#Construindo o histograma
+# Construindo o histograma
 hist(dados)
-#Os blocos do histograma são chamado de BINS
+# Os blocos do histograma são chamado de BINS
 # Os BINS são diferentes do grafico de colunas
-# Podemos alterar o tamanho dessas caixinhas com o parametro breaks
+# Podemos alterar o tamanho dessas caixinhas com o parâmetro breaks
 
 hist(dados, breaks=10, main = 'Histograma do dataset cars', labels=TRUE)
 
-# temos a mesma informaćão distribuida de uma forma diferente
+# Tenho a mesma informação distribuida de uma forma diferente
 
 hist(dados, breaks = c(0,5,10,20,30), main = 'Histogram', labels = TRUE)
 
 # Definindo o limite do eixo Y
 hist (dados, breaks=10, ylim=c(0,10), main='Histogram', labels=TRUE)
 
-#adicionando uma linha no histograma
+# Adicionando uma linha no histograma
 grafico = hist(dados, breaks=10, main='Histogram')
 xaxis = seq(min(dados), max(dados), length=10)
 yaxis = dnorm(xaxis, mean=mean(dados), sd=sd(dados))
