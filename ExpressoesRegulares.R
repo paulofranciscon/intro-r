7b185dfeb68067054d23d3b7a0a04f9d8ba01cad
Paulo Franciscon <57269765+paulofranciscon@users.noreply.github.com>
2021-09-25 17:47:11 -0300

    Update ExpressoesRegulares.R

--- ExpressoesRegulares.R
+++ ExpressoesRegulares.R
@@ -1,4 +1,4 @@
-# Funcões especiais
+# Funções especiais
 
 setwd("/media/paulo/DISC2/PUC/5 INTRODUCAO LINGUAGEM R/SCRIPTS/")
 getwd()
@@ -19,9 +19,9 @@ length(str)
 str
 
 ?grep
-#O grep questiona se há alguma palavra ou expressão que tenha o padrão 'es'
+# O grep questiona se há alguma palavra ou expressão que tenha o padrão 'es'
 grep('es', str, value=F)
-#Para trazer o elemento, valor = TRUE OU T
+# Para trazer o elemento, valor = TRUE OU T
 grep('es', str, value=TRUE)
 
 grepl('es', str)
